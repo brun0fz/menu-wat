c923a31e2eb4a54ab341c781391749b88e91711a
Bruno Fabrizio <brunofabrizio15@gmail.com>
2026-02-19 11:27:39 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import CategorySection from "./components/CategorySection";
 
 const API_URL = import.meta.env.VITE_API_URL || "";
+console.log("API_URL:", API_URL);
 
 function App() {
   const [activeCategory, setActiveCategory] = useState(null);
