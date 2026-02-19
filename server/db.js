a05cee1a33eb783fb118b3a821517950335b6a46
Bruno Fabrizio <brunofabrizio15@gmail.com>
2026-02-19 09:55:36 -0300

    support MYSQL_URL for Railway
    support MYSQL_URL for Railway

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "wat_menu",
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = process.env.MYSQL_URL
+  ? mysql.createPool(process.env.MYSQL_URL)
+  : mysql.createPool({
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME || "wat_menu",
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
 
 module.exports = pool;
